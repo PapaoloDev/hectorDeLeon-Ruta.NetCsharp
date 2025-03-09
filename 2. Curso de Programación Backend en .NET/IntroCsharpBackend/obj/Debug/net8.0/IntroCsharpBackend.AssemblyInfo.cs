ab7cfde3d25bd3552de06b3a0f16c6b5819d15c1
Paolo Pavez <ppavez.caceres@gmail.com>
2025-03-09 07:37:35 -0300

    base

--- 2. Curso de Programación Backend en .NET/IntroCsharpBackend/obj/Debug/net8.0/IntroCsharpBackend.AssemblyInfo.cs
+++ 2. Curso de Programación Backend en .NET/IntroCsharpBackend/obj/Debug/net8.0/IntroCsharpBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroCsharpBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23786e374056918c033c4655d41525feda0bac9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroCsharpBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroCsharpBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
