2c0fe7c7cd6e030edc03c6bc48e364436d4ab13c
Paolo Pavez <ppavez.caceres@gmail.com>
2025-03-09 08:48:14 -0300

    json - serialización y deserialización

--- 2. Curso de Programación Backend en .NET/IntroCsharpBackend/Program.cs
+++ 2. Curso de Programación Backend en .NET/IntroCsharpBackend/Program.cs
@@ -1,66 +1,20 @@
-﻿var numbers = new MyList<int>(5);
-var names = new MyList<string>(5);
-var beers = new MyList<Beer>(2);
+﻿using System.Text.Json;
 
-numbers.Add(1);
-numbers.Add(2);
-numbers.Add(3);
-numbers.Add(4);
-numbers.Add(5);
-numbers.Add(6);
-Console.WriteLine(numbers.GetContent());
-
-names.Add("John");
-names.Add("Doe");
-names.Add("Jane");
-names.Add("Smith");
-names.Add("Alice");
-names.Add("Bob");
-Console.WriteLine(names.GetContent());
-
-beers.Add(new Beer { Name = "Beer1", Price = 1.5 });
-beers.Add(new Beer { Name = "Beer2", Price = 2.5 });
-beers.Add(new Beer { Name = "Beer3", Price = 3.5 });
-Console.WriteLine(beers.GetContent());
-
-public class MyList<T>
+People people = new People()
 {
-    private List<T> _list;
-    private int _limit;
+    Name = "John",
+    Age = 25
+};
+string json = JsonSerializer.Serialize(people);
+Console.WriteLine(json);
 
-    public MyList(int limit)
-    {
-        _limit = limit;
-        _list = new List<T>(); // Initialize _list to avoid CS8618
-    }
+string myJson = @"{""Name"":""John"",""Age"":25}";
+People? myPeople = JsonSerializer.Deserialize<People>(myJson);
+if (myPeople != null)
+    Console.WriteLine(myPeople.Name);
 
-    public void Add(T elem)
-    {
-        if (_list.Count < _limit)
-            _list.Add(elem);
-        else
-            Console.WriteLine("List is full");
-    }
-
-    public string GetContent()
-    {
-        string content = "";
-        foreach (var elem in _list)
-        {
-            content += elem + ", ";
-        }
-
-        return content;
-    }
-}
-
-public class Beer
+public class People
 {
     public string Name { get; set; }
-    public double Price { get; set; }
-
-    public override string ToString()
-    {
-        return $"Name: {Name}, Price: {Price}";
-    }
+    public int Age { get; set; }
 }
