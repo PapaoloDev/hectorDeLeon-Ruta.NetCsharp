6e55850f2aad3914e18aacaae8672acd120b8fbf
Paolo Pavez <ppavez.caceres@gmail.com>
2025-03-11 11:31:28 -0300

    Inyección de dependencias

---
+++ 2. Curso de Programación Backend en .NET/2.Backend/Services/People2Service.cs
@@ -0,0 +1,17 @@
+﻿using _2.Backend.Controllers;
+
+namespace _2.Backend.Services
+{
+    public class People2Service : IPeopleService
+    {
+        public bool Validate(People people)
+        {
+            if (string.IsNullOrEmpty(people.Name) ||
+                people.Name.Length <= 2 ||
+                people.Name.Length > 50)
+                return false;
+            else
+                return true;
+        }
+    }
+}
--- 2. Curso de Programación Backend en .NET/2.Backend/obj/Debug/net8.0/2.Backend.AssemblyInfo.cs
+++ 2. Curso de Programación Backend en .NET/2.Backend/obj/Debug/net8.0/2.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f955ae83037da56c273b6ff694ae2c4098953a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2baaafb14f8808de8d1eef7ee00457a2cc4983be")]
 [assembly: System.Reflection.AssemblyProductAttribute("2.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
