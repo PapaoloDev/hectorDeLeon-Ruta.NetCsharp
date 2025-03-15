fe48bdb30806051a805bc5491bb660302a6d18d2
Paolo Pavez <ppavez.caceres@gmail.com>
2025-03-15 17:31:22 -0300

    validación: mensajes de error personalizados

--- 2. Curso de Programación Backend en .NET/2.Backend/Validators/BeerInsertValidator.cs
+++ 2. Curso de Programación Backend en .NET/2.Backend/Validators/BeerInsertValidator.cs
@@ -1,13 +1,15 @@
 ﻿using _2.Backend.DTOs;
 using FluentValidation;
+using System.Data;
 
 namespace _2.Backend.Validators
 {
     public class BeerInsertValidator : AbstractValidator<BeerInsertDto>
     {
         public BeerInsertValidator() {
-            RuleFor(x => x.Name).NotEmpty();
-            RuleFor(x => x.BrandId).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().WithMessage("el nombre es obligatorio.");
+            RuleFor(x => x.Name).Length(2, 20).WithMessage("el nombre debe medir de 2 a 20 caracteres.");
+            RuleFor(x => x.BrandId).NotEmpty().WithMessage("la marca es obligatoria");
             RuleFor(x => x.AlcoholPercentage).NotEmpty().InclusiveBetween(0, 100);
         }
     }
